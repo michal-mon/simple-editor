1f5c084c05ae35768b821a9ff62249aeabb5b905
Michal Mon <mmon1235@gmail.com>
2020-06-09 13:14:11 +0200

    javascript added

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,20 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+const saveButton = document.querySelector('.editor__button--save');
+
+
+saveButton.addEventListener('click', () =>{
+    const saveText = document.getElementById('text-editor').value;
+    localStorage.setItem('content', saveText);
+
+});
+
+const loadButton = document.querySelector('.editor__button--load');
+
+loadButton.addEventListener('click', ()=>{
+
+    const loadText = localStorage.getItem('content');
+    document.getElementById('text-editor').value = loadText;
+
+});
